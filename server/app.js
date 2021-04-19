694954721a738393275415996b79ae04535ae6c5
joerghebestreit <joerghebestreit@gmail.com>
2021-04-19 16:02:21 +0200

    Setup heroku

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const db = require("./lib/db");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const Post = require('./lib/models/post')
 
 /*
   We create an express app calling
   the express function.
 */
 const app = express();
+app.use(cors());
+app.use((req, res, next)=>{
+  const {method, url} = req;
+  console.log("custom middleware");
+  console.log(`${method} ${url}`);
+  next();
+})
 
 /*
   We setup middleware to:
@@ -18,16 +29,102 @@ app.use(express.json());
   Endpoint to handle GET requests to the root URI "/"
 */
 app.get("/", (req, res) => {
-  res.json({
-    "/posts": "read and create new posts",
-    "/posts/:id": "read, update and delete an individual post",
+  Post.find()
+  .then((posts) => {
+    res.status(200);
+    res.json(posts);
+  })
+  .catch((error) =>{
+    res.status(500);
+    res.json({
+      error:`Internal server error: ${error}`,
+    });
   });
 });
+
+
+app.post("/posts", (req, res) => {
+  Post.create(req.body)
+  .then(newPost => {
+    res.status(200);
+    res.json(newPost);
+  })
+  .catch(error =>{
+    res.status(500);
+    res.json({
+      error:"internal server error",
+    })
+  })
+})
+
+app.get('/posts/:id', (req, res) => {
+  const {id} = req.params;
+  Post.find()
+    .then((post) => {
+      if (post) {
+        res.status(200);
+        res.json(post);
+      } else {
+        res.status(404);
+        res.json({
+          error: `Post with id: ${id} not found`,
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({
+        error: "Internal Server Error",
+      });
+    });
+});
+
+app.patch('/posts/:id', (req, res) => {
+  const {id} = req.params;
+  Post.findByIdAndUpdate(id, req.body, {new:true})
+    .then((foundPost) => {
+        res.status(200);
+        res.json(foundPost);
+      })
+      .catch((error) => {
+        res.status(500);
+        res.json({
+          error: "Internal Server Error",
+      });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  const {id} = req.params;
+  Post.findByIdAndDelete(id)
+  .then(post => {
+    res.status(204);
+    res.json(post);
+    console.log(`Id ${id} deleted`);
+  })
+  .catch((error) =>{
+    res.status(500);
+    res.json({
+      error: "internal server error"
+    });
+  })
+})
 
 /*
   We have to start the server. We make it listen on the port 4000
 
 */
-app.listen(4000, () => {
-  console.log("Listening on http://localhost:4000");
+
+const {PORT} = process.env;
+
+mongoose.connect('mongodb://localhost/blogs', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const mongodb = mongoose.connection;
+
+mongodb.on("open", ()=> {
+  app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+  });
+  
 });
+
